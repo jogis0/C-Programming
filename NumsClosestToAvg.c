b51e2e4ffe536e4a571ae9263b47284bf083bc1b
Jokūbas Viliušis <115041388+jogis0@users.noreply.github.com>
2022-12-07 16:56:26 +0200

    Update NumsClosestToAvg.c
    Translated displayed messages to English

--- NumsClosestToAvg.c
+++ NumsClosestToAvg.c
@@ -1,9 +1,9 @@
-//Kodo autorius - Jokūbas Viliušis
-//Tai programa suskaiciuojanti skaiciu sekos vidurki ir isrenkanti 2 arciausius vidurkio skaicius: didesni ir mazesni
+//Code author - Jokūbas Viliušis
+//This program calculates the average of user-entered numbers and finds 2 closest numbers to the average: one lower than the average and one higher
 
 #include <stdio.h>
 #include <math.h>
-//Masyvo ir ivedamu skaiciu reziai
+
 #define ARRAY_MAX 1000
 #define NUM_MAX 1000000
 #define NUM_MIN (-1000000)
@@ -18,42 +18,42 @@ int avgNumMajor(int num[], int size);
 int main(){
     int numArray[ARRAY_MAX];
     int numSize;
-    printf("Ivedus N skaiciu kieki, si programa apskaiciuja N sveiku skaiciu vidurki ir isveda du arciausius vidurkio\n");
-    printf("masyvo narius: mazesni ir didesni.\n");
-    printf("Skaiciu kiekis N turi priklausyti intervalui [1;%d], sekos skaiciai turi priklausyti intervalui [%d;%d]\n", ARRAY_MAX, NUM_MIN, NUM_MAX);
+    printf("This program gets the amount of numbers to be entered (number N) and then gets N amount of numbers.\n");
+    printf("It calculates the average of the numbers and finds 2 closest numbers to the average: one lower and one higher than the average.\n");
+    printf("Number N has to be between 1 and %d, entered numbers have to be between %d and %d.\n", ARRAY_MAX, NUM_MIN, NUM_MAX);
     while(1){
-        printf("Iveskite skaiciu N:\n");
+        printf("Enter number N:\n");
         if(scanf("%d", &numSize) == 1 && getchar() == '\n'){
             if(numSize <= ARRAY_MAX && numSize >= 1){
                 break;
             }else{
-                printf("Skaiciu kiekis N turi priklausyti intervalui [1;%d].\n", ARRAY_MAX); //Jei skaicius nepriklauso nustatytam intervalui
+                printf("Number N has to be between 1 and %d.\n", ARRAY_MAX);
             }
         }else{
-            printf("Neteisinga ivestis, iveskite sveika skaiciu.\n"); //Jei ivestis nera sveikasis skaicius
+            printf("Incorrect input, input has to be a whole number.\n");
             while(getchar()!='\n')
                 ;
         }
     }
     getInputArray(numArray, numSize, NUM_MIN, NUM_MAX);
-    printf("Skaiciu sekos vidurkis: %.2lf\n", arrayAvg(numArray, numSize));
-    printf("Vidurkio mazesnysis skaicius: %d\n", avgNumMinor(numArray, numSize));
-    printf("Vidurkio didesnysis skaicius: %d\n", avgNumMajor(numArray, numSize));
+    printf("Average: %.2lf\n", arrayAvg(numArray, numSize));
+    printf("Closest number lower than the average: %d\n", avgNumMinor(numArray, numSize));
+    printf("Closest number higher than the average: %d\n", avgNumMajor(numArray, numSize));
     return 0;
 }
 void getInputArray(int num[], int size, int numMin, int numMax){
     int i = 0, temp;
     while(i<size){
-        printf("Iveskite %d-aji skaiciu:\n", i + 1);
+        printf("Enter the %d number:\n", i + 1);
         if(scanf("%d", &temp) == 1 && getchar() == '\n'){
             if(temp <= numMax && temp >= numMin){
                 num[i] = temp;
                 ++i;
             }else{
-                printf("Skaicius turi priklausyti intervalui [%d;%d].\n", numMin, numMax);
+                printf("Entered numbers have to be between %d and %d.\n", numMin, numMax);
             }
         }else{
-            printf("Neteisinga ivestis, iveskite sveika skaiciu.\n");
+            printf("Incorrect input, input has to be a whole number.\n");
             while(getchar()!='\n')
                 ;
         }
@@ -85,11 +85,11 @@ int biggestNum(int num[], int size){
     return result;
 }
 int avgNumMinor(int num[], int size){
-    int closest = smallestNum(num, size); //Pradedama nuo skaiciaus, mazesnio uz vidurki
+    int closest = smallestNum(num, size);
     double closestDiff = fabs(arrayAvg(num, size) - closest);
     for(int i=0; i<size; ++i){
         double currentDiff = fabs(arrayAvg(num, size) - num[i]);
-        if(currentDiff < closestDiff && num[i] < arrayAvg(num, size)){ //Ziurime, ar kiekvieno nario skirtumas yra mazesnis, uz maziausia skirtuma
+        if(currentDiff < closestDiff && num[i] < arrayAvg(num, size)){
             closest = num[i];
             closestDiff = currentDiff;
         }
